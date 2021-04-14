dc9b42bcf60f196c1ec03f072aa42abdf860fad2
SCRIPTS 2021 <69657855+lacasitamx@users.noreply.github.com>
2021-04-14 11:59:55 -0600

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 clear
+echo "$HOME/bbr.sh" >/bin/tcpbbr
 sh_ver="1.3.1"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[Informacion]${Font_color_suffix}"
