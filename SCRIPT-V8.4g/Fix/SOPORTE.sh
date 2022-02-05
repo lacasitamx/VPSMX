c00443ac4619ce39889af598bb9f348ab273f3e1
MX-2021-2022 <69657855+lacasitamx@users.noreply.github.com>
2022-02-04 22:01:25 -0700

    Update SOPORTE.sh

--- SCRIPT-V8.4g/Fix/SOPORTE.sh
+++ SCRIPT-V8.4g/Fix/SOPORTE.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+#Soporte Remoto (SPR)
+echo -e "\033[0;37m BIENVENIDO A SOPORTE ONLINE (APLICANDO FIX's)8.4g-5-2-22"
 
+export LANGUAGE=en_US.UTF-8\
+  && export LANG=en_US.UTF-8\
+  && export LC_ALL=en_US.UTF-8\
+  && export LC_CTYPE="en_US.UTF-8"\
+  && locale-gen en_US.UTF-8\
+  && sudo apt-get -y install language-pack-en-base\
+  && sudo dpkg-reconfigure locales
+
+echo -e "\033[0;92m Parche Online Aplicado.... OK"
